0903b05e9e897f97c113039aaba9cf1aaa995c62
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-07-08 17:47:05 -0400

    Complete iteration 2

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+  return shelf;
+}
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      var bookIndex = shelf.indexOf(shelf[i]);
+      shelf.splice(bookIndex, 1);
+    }
+  }
+  return shelf;
+}
+function listTitles(shelf) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  titles = titles.toString().replaceAll(",", ", ");
+  return titles;
+}
+
+function searchShelf(shelf, book) {
+  var onShelf;
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      onShelf = true;
+    }
+    else if (book !== shelf[i].title) {
+      onShelf = false;
+    }
+  }
+  return onShelf;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
